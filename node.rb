92a1a5bed55e5c72cef4c85406f58da0f26a5010
David Keller <dckeller1@gmail.com>
2017-12-01 20:08:32 -0700

    Refactor

--- node.rb
+++ node.rb
@@ -1,3 +1,16 @@
 class Node
+	attr_accessor :value, :node_after
+
+	def initialize(value, node_after)
+		@value = value
+		@node_after = node_after
+	end
+
+	def insert_after(other_node)
+
+	end
+
+	def remove_after()
+	end   
 
 end
